04a26bbba28a621b02253182674ca365c0f63f8a
Riyanto Widodo <43575837+69slimm@users.noreply.github.com>
2024-02-08 14:42:39 +0700

    Update prima.sh

--- prima.sh
+++ prima.sh
@@ -4,13 +4,14 @@ prima=$1
 
 count=2
 
-while [ $count -lt $prima ]; do
+if [ $count -lt $prima ];then
   if [ `expr $prima % $count` -eq 0 ]; then
      echo "bilangan ini bukan bilangan prima"
-     exit 0
-   fi
-   
-   count=`expr $count + 1`
-done
+     exit 0;
+  else
+    echo "bilangan ini adalah bilangan prima"
+  fi
+else
+  echo "angka harus lebih dari 2"
+fi
 
-echo "bilangan ini adalah bilangan prima"
