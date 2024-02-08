6987e575690b606f9e7121bbf0de853c3917a41c
Riyanto Widodo <43575837+69slimm@users.noreply.github.com>
2024-02-08 14:55:21 +0700

    Update prima.sh

--- prima.sh
+++ prima.sh
@@ -2,10 +2,10 @@
 
 prima=$1
 
-count=2
+patokan=2
 
-if [ $count -lt $prima ];then
-  if [ `expr $prima % $count` -eq 0 ]; then
+if [ $patokan -lt $prima ];then
+  if [ `expr $prima % $patokan` -eq 0 ]; then
      echo "bilangan ini bukan bilangan prima"
      exit 0;
   else
@@ -14,4 +14,3 @@ if [ $count -lt $prima ];then
 else
   echo "angka harus lebih dari 2"
 fi
-
