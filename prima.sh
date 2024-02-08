6bbb4e1f674e624c31f5c0d262283704215ae84c
Riyanto Widodo <43575837+69slimm@users.noreply.github.com>
2024-02-08 17:08:08 +0700

    Add files via upload

--- prima.sh
+++ prima.sh
@@ -1,16 +1,12 @@
-#!/bin/bash
+n=$1
 
-prima=$1
-
-patokan=2
-
-if [ $patokan -lt $prima ];then
-  if [ `expr $prima % $patokan` -eq 0 ]; then
-     echo "bilangan ini bukan bilangan prima"
-     exit 0;
-  else
-    echo "bilangan ini adalah bilangan prima"
-  fi
-else
-  echo "angka harus lebih dari 2"
-fi
+for((i=2; i<=$n/2; i++))
+do
+  ans=$(( n%i ))
+    if [ $ans -eq 0 ]
+      then
+          echo "bilangan ini bukan bilangan prima"
+	  exit 0;
+    fi
+done
+echo "bilangan ini adalah bilangan prima"
